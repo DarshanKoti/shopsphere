cd6182022688e464a6ce60e06f735dd726c12771
DarshanKoti <darshankoti40@gmail.com>
2026-09-08 05:21:56 +0530

    fix: revert login changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
-import Contact from "./pages/Contact";
 import Profile from "./pages/Profile";
-import Navbar from "./components/layouts/Navbar";
-import Footer from "./components/layouts/Footer";
+import Contact from "./pages/Contact";
 import Construction from "./pages/Construction";
-import Wishlist from "./components/sections/ProfileSections/Wishlist";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <>
       <Navbar />
 
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />{" "}
-          <Route path="/products/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/coming-soon" element={<Construction />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-        </Routes>
-      </main>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/coming-soon" element={<Construction />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
